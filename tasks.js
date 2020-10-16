04773ff20e04722c035563c597f648ff2793e7cf
Dmitry Shaplyko <dmitryshaplyko@MacBook-Pro.local>
2020-10-16 08:54:34 +0300

    tasks added

---
+++ tasks.js
@@ -0,0 +1,28 @@
+/**
+ * Make a copy of the provided array
+ */
+const arr = [1,2,3,4,5];
+
+/**
+ * Create a function that checks if the given string is palyndrome
+ */
+
+ function isPalyndrome(str) {
+
+ }
+
+ isPalyndrome('kayak'); //true
+ isPalyndrome(1001); //true
+ isPalyndrome('test'); //false
+
+ /**
+  * Print in the console the index of the minimal item from the given array
+  */
+
+const numbers = [2,33,5,3,1,55,98];
+
+function findMinIndex(arr) {
+
+}
+
+findMinIndex(numbers); //should print 4
